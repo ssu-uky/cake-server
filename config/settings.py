a8fb3a8ab2bacab3916be2cd7b53be30a7d5cac8
ssu-uky <id_suhyun@naver.com>
2023-05-22 00:34:33 +0900

    database

--- config/settings.py
+++ config/settings.py
@@ -94,15 +94,15 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
-# DATABASES = my_settings.MY_DATABASES
+DATABASES = my_settings.MY_DATABASES
 
 
 # Password validation
